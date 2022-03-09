9860d06b75b92f594240df29252ed635db80c196
Robin Lieb <robin.j.lieb@gmail.com>
2022-03-09 20:58:30 +0100

    feat: added UI and persistance of max tabs number

--- src/popup.js
+++ src/popup.js
@@ -6,6 +6,8 @@ function storeSettings() {
     } else {
         browser.storage.local.set({ "enableBadge": false });
     }
+    var maxTabs = document.getElementById("max_tabs").value;
+    browser.storage.local.set({ "maxTabs": maxTabs });
     browser.runtime.sendMessage({ type: "Settings changed" });
 }
 
@@ -14,9 +16,13 @@ function restoreDefaults() {
     browser.storage.local.get("enableBadge").then(function (value) {
         document.getElementById("show_badge_checkbox_btn").checked = value.enableBadge;
     });
+    browser.storage.local.get("maxTabs").then(function (value) {
+        document.getElementById("max_tabs").value = parseInt(value.maxTabs);
+    });
 }
 
 // Add event listener for UI settings selements
 document.getElementById('show_badge_checkbox_btn').addEventListener('change', storeSettings);
+document.getElementById('max_tabs').addEventListener('change', storeSettings);
 
 restoreDefaults()
