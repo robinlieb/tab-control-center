0d7bd20f0d914682245d5544d0f65fb5af66be77
Robin Lieb <robin.j.lieb@gmail.com>
2022-03-11 13:28:13 +0100

    build: added webpack and web-ext for build and package the application

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: {
+    background: "./background_scripts/background.js",
+    content: "./content_scripts/content.js",
+    popup: "./popup/popup.js"
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+  },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: "./manifest.json" },
+        { from: "./popup/popup.css" },
+        { from: "./popup/popup.html" },
+        { from: "./icons", to: "icons" },
+        { from: "./_locales", to: "_locales" },
+        { from: "./manifest.json" },
+        { from: './node_modules/webextension-polyfill/dist/browser-polyfill.js' },
+      ],
+    }),
+    ]
+};
