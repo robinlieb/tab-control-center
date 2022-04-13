bf9b726fb00630536b82ccdb03b2f269027dce58
Robin Lieb <robin.j.lieb@gmail.com>
2022-04-13 16:34:56 +0200

    build: apply browser specific maifest via build script

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,30 @@
 const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-module.exports = {
-  entry: {
-    background: "./background_scripts/background.js",
-    popup: "./popup/popup.js"
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].js',
-  },
-  plugins: [
-    new CopyWebpackPlugin({
-      patterns: [
-        { from: "./manifest.json" },
-        { from: "./popup/popup.css" },
-        { from: "./popup/popup.html" },
-        { from: "./icons", to: "icons" },
-        { from: "./_locales", to: "_locales" },
-        { from: "./manifest.json" },
-        { from: './node_modules/webextension-polyfill/dist/browser-polyfill.js' },
-      ],
-    }),
+module.exports = (env) => {
+  console.log(env)
+  console.log(env.browser)
+  const config = {
+    entry: {
+      background: "./background_scripts/background.js",
+      popup: "./popup/popup.js"
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: '[name].js',
+    },
+    plugins: [
+      new CopyWebpackPlugin({
+        patterns: [
+          { from: `./manifest/${env.browser}.json`, to: "manifest.json" },
+          { from: "./popup/popup.css" },
+          { from: "./popup/popup.html" },
+          { from: "./icons", to: "icons" },
+          { from: "./_locales", to: "_locales" },
+          { from: './node_modules/webextension-polyfill/dist/browser-polyfill.js' },
+        ],
+      }),
     ]
+  }
+  return config
 };
